4fe5b2d689cd88a8807fe1b21480f8f0bde4e996
Jorge Gabitto <jgabitto1792@gmail.com>
2020-10-11 16:22:48 -0500

    Added message rendering on modal

--- src/components/landingPage/ForgotPassword.js
+++ src/components/landingPage/ForgotPassword.js
@@ -122,7 +122,7 @@
 import React, { useState } from 'react';
 import { Button, Modal, Form, Input, Row } from 'antd';
 
-const ForgotPasswordForm = ({ visible, onSubmit, onCancel }) => {
+const ForgotPasswordForm = ({ visible, message, onSubmit, onCancel }) => {
   const [form] = Form.useForm();
   return (
     <Modal
@@ -143,49 +143,64 @@ const ForgotPasswordForm = ({ visible, onSubmit, onCancel }) => {
           });
       }}
     >
-      <Row type="flex" align="center" >
-        Enter your user account's verified email address and we will send you a password reset link.
+      { message ? message.error :
+        <>
+          <Row type="flex" align="center" >
+            Enter your user account's verified email address and we will send you a password reset link.
       </Row>
-      <Form
-        form={form}
-        layout="vertical"
-        name="form_in_modal"
-        initialValues={{
-          modifier: 'public',
-        }}
-      >
-        <Form.Item
-          name="email"
-          style={{ paddingTop: "10px" }}
-          rules={[
-            {
-              type: 'email',
-              message: 'The input is not valid E-mail!',
-            },
-            {
-              required: true,
-              message: 'Please input your email',
-            }
-          ]}
-        >
-          <Input placeholder="johndoe@email.com" />
-        </Form.Item>
-      </Form>
+          <Form
+            form={form}
+            layout="vertical"
+            name="form_in_modal"
+            initialValues={{
+              modifier: 'public',
+            }}
+          >
+            <Form.Item
+              name="email"
+              style={{ paddingTop: "10px" }}
+              rules={[
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your email',
+                }
+              ]}
+            >
+              <Input placeholder="johndoe@email.com" />
+            </Form.Item>
+          </Form>
+        </>
+      }
     </Modal>
   );
 };
 
 const ForgotPassword = () => {
-  const [state, setState] = useState({ visible: false, email: null });
-  const { visible, email } = state;
+  const [state, setState] = useState({ visible: false, email: null, message: null });
+  const { visible, email, message } = state;
 
   // const onChange = () => {
   //   setState({ visible: false, email });
   // }
 
-  const onSubmit = (email) => {
+  const onSubmit = async (email) => {
     console.log('Received values of form: ', email);
-    setState({ visible: false, email });
+    try {
+      const res = await fetch("https://reqres.in/api/login", {
+        method: "POST",
+        body: JSON.stringify(email),
+        headers: { "Content-Type": "application/json" }
+      });
+      const message = await res.json();
+      console.log(message)
+      setState({ visible: true, email, message });
+    } catch (e) {
+      console.log(e.message)
+    }
   };
 
   return (
@@ -199,11 +214,12 @@ const ForgotPassword = () => {
         Forgot Password
       </Button>
       <ForgotPasswordForm
+        message={message}
         email={email}
         visible={visible}
         onSubmit={onSubmit}
         onCancel={() => {
-          setState({ visible: false });
+          setState({ visible: false, message });
         }}
       />
     </div>
