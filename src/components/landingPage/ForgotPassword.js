c561a7045ebee98054c7fb1295d4234599b2e282
Jorge Gabitto <jgabitto1792@gmail.com>
2020-10-11 13:57:38 -0500

    Started forgot password modal

--- src/components/landingPage/ForgotPassword.js
+++ src/components/landingPage/ForgotPassword.js
@@ -1,125 +1,141 @@
-import React, { useState } from 'react';
-import { Spin, Button, Modal, Form, Input, Row } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
+import React, { useState, useRef } from 'react';
+import { Button, Modal, Form, Input, Row } from 'antd';
 
-const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
-
-const ForgotPasswordForm = ({ visible, message, email, onSubmit, onCancel }) => {
-  const [form] = Form.useForm();
-
-  const onOk = () => {
-    form
-      .validateFields()
-      .then((values) => {
-        form.resetFields();
-        onSubmit(values);
-      })
-      .catch((info) => {
-
-      });
-  }
-
-  return (
-    <Modal
-      visible={visible}
-      title="Reset your password"
-      onCancel={onCancel}
-      footer={[
-        <Button key="back" onClick={onCancel}>
-          Cancel
-        </Button>,
-        <Button key="submit" type="primary" disabled={email} onClick={onOk}>
-          Submit
-        </Button>
-      ]}
-    >
-      { message
-        ? message.error
-        : <>
-          { email
-            ? <Row type="flex" align="center">
-              <Spin indicator={antIcon} />
-            </Row>
-            :
-            <>
-              <Row type="flex" align="center" >
-                <strong>Enter your user account's verified email address and we will send you a password reset link.</strong>
-                </Row>
-              <Form
-                form={form}
-                layout="vertical"
-                name="form_in_modal"
-                initialValues={{
-                  modifier: 'public',
-                }}
-              >
-                <Form.Item
-                  name="email"
-                  style={{ paddingTop: "10px" }}
-                  rules={[
-                    {
-                      type: 'email',
-                      message: 'The input is not valid E-mail!',
-                    },
-                    {
-                      required: true,
-                      message: 'Please input your email',
-                    }
-                  ]}
-                >
-                  <Input placeholder="johndoe@email.com" />
-                </Form.Item>
-              </Form>
-            </>
-          }
-        </>
-      }
-    </Modal>
-  );
+const layout = {
+  wrapperCol: {
+    span: 24,
+  },
 };
 
 const ForgotPassword = () => {
-  const [state, setState] = useState({ visible: false, email: null, message: null });
-  const { visible, email, message } = state;
+  const [state, setState] = useState({ ModalText: 'Content of the modal', visible: false, confirmLoading: false, });
+  const stateRef = useRef(state);
+  stateRef.current = state;
+  const { ModalText, visible, confirmLoading } = state;
 
-  const onSubmit = async (email) => {
-    try {
-      setState({ email, visible: true })
-      const res = await fetch("https://reqres.in/api/login", {
-        method: "POST",
-        body: JSON.stringify(email),
-        headers: { "Content-Type": "application/json" }
-      });
-      const message = await res.json();
+  console.log(state)
 
-      setState({ email, visible: true, message });
-    } catch (e) {
-      setState({ email, visible: true, message: e.message });
-    }
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const showModal = () => {
+    setState({ visible: true });
+  };
+
+  const handleOk = () => {
+    setState({
+      ModalText: 'The modal will be closed after two seconds',
+      confirmLoading: true,
+    })
+  };
+
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setState({
+      visible: false,
+    });
   };
 
   return (
-    <div>
-      <Button
-        type="link"
-        onClick={() => {
-          setState({ visible: true });
-        }}
-      >
-        Forgot Password
-      </Button>
-      <ForgotPasswordForm
-        message={message}
-        email={email}
+    <>
+      <Button type="primary" onClick={showModal}>
+        Open Modal with async logic
+        </Button>
+      <Modal
+        title="Forgot Password"
         visible={visible}
-        onSubmit={onSubmit}
-        onCancel={() => {
-          setState({ visible: false, message });
-        }}
-      />
-    </div>
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <Row type="flex" justify="center" >
+          <Row type="flex" align="center" >
+            Enter your user account's verified email address and we will send you a password reset link.
+          </Row>
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your email!',
+                },
+              ]}
+            >
+              <Input placeholder="Enter email" />
+            </Form.Item>
+          </Form>
+        </Row>
+      </Modal>
+    </>
   );
-};
+}
 
+// class ForgotPassword extends React.Component {
+//   state = {
+//     ModalText: "Content of the modal",
+//     visible: false,
+//     confirmLoading: false
+//   };
+
+//   showModal = () => {
+//     this.setState({
+//       visible: true
+//     });
+//   };
+
+//   handleOk = () => {
+//     this.setState({
+//       ModalText: "The modal will be closed after two seconds",
+//       confirmLoading: true
+//     });
+//     setTimeout(() => {
+//       this.setState({
+//         visible: false,
+//         confirmLoading: false
+//       });
+//     }, 2000);
+//   };
+
+//   handleCancel = () => {
+//     console.log("Clicked cancel button");
+//     this.setState({
+//       visible: false
+//     });
+//   };
+
+//   render() {
+//     const {visible, confirmLoading, ModalText} = this.state;
+//     return (
+//       <>
+//         <Button type="primary" onClick={this.showModal}>
+//           Open Modal with async logic
+//         </Button>
+//         <Modal
+//           title="Title"
+//           visible={visible}
+//           onOk={this.handleOk}
+//           confirmLoading={confirmLoading}
+//           onCancel={this.handleCancel}
+//         >
+//           <p>{ModalText}</p>
+//         </Modal>
+//       </>
+//     );
+//   }
+// }
 
 export default ForgotPassword;
