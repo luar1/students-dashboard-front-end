72059667ab0b4d6e43faaadc1776068be57b035c
meljoi <uradgal@gmail.com>
2020-10-11 13:15:47 -0400

    login in form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
-import Main from './components/main';
+import Login from "./components/Login"
+
 
 function App() {
   return (
-    <Main />
+    <div className= "container center">
+
+    <Login />
+    </div>
   );
 }
 
