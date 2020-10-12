6f83c504ffddec6625db2e644e29e89320c1493e
Jorge Gabitto <jgabitto1792@gmail.com>
2020-10-12 09:00:56 -0500

    Added spinner to modal

--- src/components/landingPage/ForgotPassword.js
+++ src/components/landingPage/ForgotPassword.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { Button, Modal, Form, Input, Row } from 'antd';
+import { Spin, Button, Modal, Form, Input, Row } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
-const ForgotPasswordForm = ({ visible, message, onSubmit, onCancel }) => {
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+const ForgotPasswordForm = ({ visible, message, email, onSubmit, onCancel }) => {
   const [form] = Form.useForm();
   return (
     <Modal
@@ -22,36 +25,45 @@ const ForgotPasswordForm = ({ visible, message, onSubmit, onCancel }) => {
           });
       }}
     >
-      { message ? message.error :
-        <>
-          <Row type="flex" align="center" >
-            Enter your user account's verified email address and we will send you a password reset link.
-      </Row>
-          <Form
-            form={form}
-            layout="vertical"
-            name="form_in_modal"
-            initialValues={{
-              modifier: 'public',
-            }}
-          >
-            <Form.Item
-              name="email"
-              style={{ paddingTop: "10px" }}
-              rules={[
-                {
-                  type: 'email',
-                  message: 'The input is not valid E-mail!',
-                },
-                {
-                  required: true,
-                  message: 'Please input your email',
-                }
-              ]}
-            >
-              <Input placeholder="johndoe@email.com" />
-            </Form.Item>
-          </Form>
+      { message
+        ? message.error
+        : <>
+          { email
+            ? <Row type="flex" align="center">
+              <Spin indicator={antIcon} />
+            </Row>
+            :
+            <>
+              <Row type="flex" align="center" >
+                Enter your user account's verified email address and we will send you a password reset link.
+                </Row>
+              <Form
+                form={form}
+                layout="vertical"
+                name="form_in_modal"
+                initialValues={{
+                  modifier: 'public',
+                }}
+              >
+                <Form.Item
+                  name="email"
+                  style={{ paddingTop: "10px" }}
+                  rules={[
+                    {
+                      type: 'email',
+                      message: 'The input is not valid E-mail!',
+                    },
+                    {
+                      required: true,
+                      message: 'Please input your email',
+                    }
+                  ]}
+                >
+                  <Input placeholder="johndoe@email.com" />
+                </Form.Item>
+              </Form>
+            </>
+          }
         </>
       }
     </Modal>
@@ -65,6 +77,7 @@ const ForgotPassword = () => {
   const onSubmit = async (email) => {
     console.log('Received values of form: ', email);
     try {
+      setState({ email, visible: true })
       const res = await fetch("https://reqres.in/api/login", {
         method: "POST",
         body: JSON.stringify(email),
@@ -72,7 +85,7 @@ const ForgotPassword = () => {
       });
       const message = await res.json();
       console.log(message)
-      setState({ visible: true, email, message });
+      setState({ visible: true, message });
     } catch (e) {
       console.log(e.message)
     }
