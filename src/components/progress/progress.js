c539b3e190ca3e03f557df0f3abae368ac5f748e
NATALIA ESPAÑA <natacski@gmail.com>
2020-11-24 16:19:13 -0500

    Components fetching data from airtable.

--- src/components/progress/progress.js
+++ src/components/progress/progress.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { Progress, Card, Row, Col, Space, Typography } from "antd";
+import { Link } from "react-router-dom";
 
 const MainProgress = () => {
+  
   //Fetching data from airtable
   const getMentorsData = async () => {
     const response = await fetch(
-      "https://api.airtable.com/v0/appm5NPkqO7P8ePUK/Progress?api_key=keyclOytaXo7NHQ8M"
+      "https://api.airtable.com/v0/appm5NPkqO7P8ePUK/Progress?api_key=keyclOytaXo7NHQ8M&sort%5B0%5D%5Bfield%5D=id&sort%5B0%5D%5Bdirection%5D=asc"
     );
     const progressData = await response.json();
     return progressData;
   };
 
-  const [progress, setProgress] = useState([]);
+  const [progress, setProgress] = useState(null);
 
   useEffect(() => {
     getMentorsData().then((data) => setProgress(data));
@@ -27,96 +29,40 @@ const MainProgress = () => {
           </Typography.Title>
           <div className="progress">
             <Row gutter={24}>
-              <Col flex="1 1 ">
-                <Card
-                  style={{
-                    height: 255,
-                    textAlign: "center",
-                    paddingTop: 20,
-                    marginBottom: 20,
-                  }}
-                  cover={
-                    <Progress
-                      type="circle"
-                      strokeColor={{
-                        "0%": "#108ee9",
-                        "100%": "#87d068",
-                      }}
-                      percent={100}
-                    />
-                  }
-                >
-                  <Meta title="Week 1" description="This is the description" />
-                </Card>
-              </Col>
-              <Col flex="1 1 ">
-                <Card
-                  style={{
-                    height: 255,
-                    textAlign: "center",
-                    paddingTop: 20,
-                    marginBottom: 20,
-                  }}
-                  cover={
-                    <Progress
-                      type="circle"
-                      strokeColor={{
-                        "0%": "#108ee9",
-                        "100%": "#87d068",
-                      }}
-                      percent={70}
-                    />
-                  }
-                >
-                  <Meta title="Week 2" description="This is the description" />
-                </Card>
-              </Col>
-              <Col flex="1 0 ">
-                {" "}
-                <Card
-                  style={{
-                    height: 255,
-                    textAlign: "center",
-                    paddingTop: 20,
-                    marginBottom: 20,
-                  }}
-                  cover={
-                    <Progress
-                      type="circle"
-                      strokeColor={{
-                        "0%": "#108ee9",
-                        "100%": "#87d068",
-                      }}
-                      percent={0}
-                    />
-                  }
-                >
-                  <Meta title="Week 3" description="This is the description" />
-                </Card>
-              </Col>
-              <Col flex="1 0 ">
-                {" "}
-                <Card
-                  style={{
-                    height: 255,
-                    textAlign: "center",
-                    paddingTop: 20,
-                    marginBottom: 20,
-                  }}
-                  cover={
-                    <Progress
-                      type="circle"
-                      strokeColor={{
-                        "0%": "#108ee9",
-                        "100%": "#87d068",
-                      }}
-                      percent={0}
-                    />
-                  }
-                >
-                  <Meta title="Week 4" description="This is the description" />
-                </Card>
-              </Col>
+              {progress
+                ? progress.records.map((week) => {
+                    return (
+                      <Col flex="1 1 ">
+                        <Card
+                          style={{
+                            height: 255,
+                            textAlign: "center",
+                            paddingTop: 20,
+                            marginBottom: 20,
+                          }}
+                          cover={
+                            <Link to="/home/dashboard/assignments/">
+                              {" "}
+                              <Progress
+                                type="circle"
+                                strokeColor={{
+                                  "0%": "#108ee9",
+                                  "100%": "#87d068",
+                                }}
+                                percent={week.fields.Progress}
+                              />
+                            </Link>
+                          }
+                        >
+                          <Meta
+                            title={week.fields.Unit}
+                            description="This is the description"
+                          />
+                        </Card>
+                      </Col>
+                    );
+                  })
+                : null}
             </Row>
           </div>
         </Card>
--- src/components/todoList/todoList.js
+++ src/components/todoList/todoList.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Card, Typography } from "antd";
+import { Card, Typography, Checkbox } from "antd";
 import { CheckSquareFilled, CloseSquareFilled } from "@ant-design/icons";
-
-const Todo = ({ todo, id, completeTodo, removeTodo }) => {
+const Todo = ({ todo, completeTodo }) => {
   const boxesStyle = {
     marginRight: 5,
     color: "#0275D8",
@@ -10,53 +9,18 @@ const Todo = ({ todo, id, completeTodo, removeTodo }) => {
     paddingLeft: 8,
   };
   return (
-    <div style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+    <div style={{ textDecoration: todo.Status ? "line-through" : "" }}>
       <div>
-        <button
-          //className="todo-buttons"
-          //
-          style={boxesStyle}
-          onClick={() => completeTodo(todo.id)}
-        >
-          <CheckSquareFilled />
-        </button>
+        <Checkbox
+          checked={todo.fields.Status ? true : null}
+          onChange={() => completeTodo(todo.id, !todo.fields.Status)}
+        />
         {todo.fields.ToDo}
-        {/* <button style={boxesStyle} onClick={() => removeTodo(todo.id)}>
-          <CloseSquareFilled />
-        </button> */}
       </div>
     </div>
   );
 };
-// const TodoForm = ({ addTodo }) => {
-//   const todoInput = {
-//     borderRadius: 10,
-//     border: "1px solid #0275D8",
-//     width: 160,
-//     marginBottom: 20
-//   };
-//   const [value, setValue] = useState("");
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (!value) return;
-//     addTodo(value);
-//     setValue("");
-//   };
-//   return (
-//     <>
-//       <h4>Add New</h4>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           //className="todo-input"
-//           style={todoInput}
-//           value={value}
-//           onChange={(e) => setValue(e.target.value)}
-//         />
-//       </form>
-//     </>
-//   );
-// };
+
 const ToDoList = () => {
   const getToDoData = async () => {
     const response = await fetch(
@@ -69,35 +33,36 @@ const ToDoList = () => {
   useEffect(() => {
     getToDoData().then((data) => setTodos(data.records));
   }, []);
-  // const addTodo = (text) => {
-  //   //this cannot work now that we are connected to airtable
-  //   //const newTodos = [...todos, { text }];
-  //   //setTodos(newTodos);
-  // };
-  const completeTodo = (id) => {
-    //const newTodos = [...todos];
-    //newTodos[id].isCompleted = true;
-    //setTodos(newTodos);
+
+  const completeTodo = (id, status) => {
+    fetch("https://api.airtable.com/v0/appm5NPkqO7P8ePUK/List", {
+      body: JSON.stringify({
+        records: [
+          {
+            id: id,
+            fields: {
+              Status: status,
+            },
+          },
+        ],
+      }),
+      headers: {
+        Authorization: "Bearer keyclOytaXo7NHQ8M",
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+    })
+      .then(() => {
+        getToDoData().then((data) => setTodos(data.records));
+      })
+      .catch((e) => {
+        console.log(e);
+        alert("Unable to update to do ");
+      });
   };
-  // const removeTodo = (id) => {
-  //   setTodos(todos.filter((item) => item.id !== id));
-  //   //you also need to delete the item from airtable
-  // };
+
   return (
     <>
-      {/* <Row>
-        <Col span={4}></Col>
-        <Col justify="center" xs={24} sm={24} md={24} lg={10} xl={8} xxl={6}> */}
-      {/* <div
-            style={{
-              padding: 20,
-              border: "1 solid #D3D3D3",
-              borderRadius: 10,
-              width: 250,
-              background: "#F1F1F2"
-              //boxShadow: 0 3 "#C8C8C8",
-            }}
-          > */}
       <Card
         className="shadow center"
         style={{
@@ -105,10 +70,9 @@ const ToDoList = () => {
           padding: 0,
           border: "1px solid #D3D3D3",
           borderRadius: 6,
-          background: "#f1f1f2",
+          background: "#F1F1F2",
         }}
       >
-        {/* <h2 style={{ marginTop: 20 }}>To Do</h2> */}
         <Typography.Title level={4}>To Do</Typography.Title>
         {todos &&
           todos.map((todo, index) => (
@@ -116,19 +80,12 @@ const ToDoList = () => {
               key={index}
               id={todo.id}
               todo={todo}
-              //completeTodo={completeTodo}
-              //removeTodo={removeTodo}
+              completeTodo={completeTodo}
             />
           ))}
         <br></br>
-        {/* <TodoForm addTodo={addTodo} /> */}
       </Card>
-      {/* </Col>
-        <Col span={4}></Col>
-      </Row> */}
     </>
   );
 };
 export default ToDoList;
-
-
